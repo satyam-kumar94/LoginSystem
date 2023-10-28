fe3c408b2d7f87c28330c34b44499a584ef02e17
satyam <tech.kumarsatyam@gmail.com>
2023-10-28 16:39:20 +0530

    remove middile page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import Signup from "../Signup/Signup";
 
 function Home(props) {
   return (
-    <div>
-      <div>
-        <h1>
-          <Link to="/login">Login</Link>
-        </h1>
-        <br />
-        <h1>
-          <Link to="/signup">Signup</Link>
-        </h1>
-      </div>
-
-      <br />
-      <br />
-      <br />
-
-      <h2>{props.name ? `Welcome - ${props.name}` : "Login please"}</h2>
-    </div>
+   <div><Signup /></div>
   );
 }
 
